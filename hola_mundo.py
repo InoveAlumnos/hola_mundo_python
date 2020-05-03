f8a293f8e5247badb98ab65a0947c515f3d3f97a
InoveAlumnos <64550682+InoveAlumnos@users.noreply.github.com>
2020-05-03 16:41:10 -0300

    Create hola_mundo.py

---
+++ hola_mundo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+'''
+Hola Mundo! [Python]
+---------------------------
+Autor: Inove Coding School
+Version: 1.2
+ 
+Descripcion:
+Programa creado para ensayar el correcto funcionamiento del entorno de instalación Python
+'''
+
+__author__ = "Inove Coding School"
+__email__ = "alumnos@inove.comar.ar"
+__version__ = "1.2"
+
+print("Hola Mundo!")
