3a5df32d2b55c0322ba46a7a6f8c1ae9749a3bd8
InoveAlumnos <64550682+InoveAlumnos@users.noreply.github.com>
2020-05-10 09:18:20 -0300

    Update hola_mundo.py
    Typo en la dirección de email

--- hola_mundo.py
+++ hola_mundo.py
@@ -3,14 +3,14 @@
 Hola Mundo! [Python]
 ---------------------------
 Autor: Inove Coding School
-Version: 1.2
+Version: 1.3
  
 Descripcion:
 Programa creado para ensayar el correcto funcionamiento del entorno de instalación Python
 '''
 
 __author__ = "Inove Coding School"
-__email__ = "alumnos@inove.comar.ar"
-__version__ = "1.2"
+__email__ = "alumnos@inove.com.ar"
+__version__ = "1.3"
 
 print("Hola Mundo!")
