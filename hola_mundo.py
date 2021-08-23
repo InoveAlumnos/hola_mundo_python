1b97b7ef84b69d4e9668cce869004eda57b8ddf2
hernancontigiani <hernan4790@gmail.com>
2021-08-23 08:43:37 -0300

    Reemplazar comentarios por #

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,17 +1,11 @@
-#!/usr/bin/env python
-'''
-Hola Mundo! [Python]
----------------------------
-Autor: Inove Coding School
-Version: 1.3
+# Hola Mundo! [Python]
+# ---------------------------
+# Autor: Inove Coding School
+# Version: 2.0
 
-Descripcion:
-Programa creado para ensayar el correcto funcionamiento
-del entorno de instalación Python
-'''
+# Descripcion:
+# Programa creado para ensayar el correcto funcionamiento
+# del entorno de instalación Python
 
-__author__ = "Inove Coding School"
-__email__ = "alumnos@inove.com.ar"
-__version__ = "1.3"
 
 print("Hola Mundo!")
